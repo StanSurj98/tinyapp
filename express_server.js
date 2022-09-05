5bb7bcc2fd5e4dbd51f9bfba33ebdda9db035ed4
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-05 14:53:43 -0600

    added new endpoint at /urls.json -> shows serialized json of the urlDatabase variable

--- express_server.js
+++ express_server.js
@@ -12,6 +12,11 @@ app.get("/", (req, res) => {
   res.send(`Hello!`);
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase); // Takes the urlDatabase and display serialized json at this path
+});
+
+
 // This tells our server to listen on our port
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}!`);
