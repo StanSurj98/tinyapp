331a2b7304692c30d0e1c7f9a5914a79a9ab34b4
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-05 14:47:45 -0600

    Created localhost express server on port 8080.

---
+++ express_server.js
@@ -0,0 +1,18 @@
+const express = require('express'); // Imports the express module
+const app = express();
+const PORT = 8080; // Default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+// Get request to root path "/"
+app.get("/", (req, res) => {
+  res.send(`Hello!`);
+});
+
+// This tells our server to listen on our port
+app.listen(PORT, () => {
+  console.log(`Example app listening on port: ${PORT}!`);
+});
