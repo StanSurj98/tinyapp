c57eb5c63dcd486465e057b658f35a499ecc3d9d
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-05 16:34:27 -0600

    created urls_show.ejs for showing the long URL and its shortened version. Added this endpoint in the express_server.js file

--- express_server.js
+++ express_server.js
@@ -24,12 +24,19 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase); // Takes the urlDatabase and display serialized json at this path
 });
 
-app.get("urls", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; // when using EJS template, MUST pass an object
   // EJS knows to look inside a "views" dir automatically for a "urls_index.ejs" file
   res.render("urls_index", templateVars); // that's why we don't need extension or path
 });
 
+// New endpoint to page showing shortened URL. "/:id" -> ROUTE parameter the id is added to req.params.id in express and will display as the unique shortened id in the search bar
+app.get("/urls/:id", (req, res) => {
+  // fetches the long URL that matches the short id url
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  // pass this data onto the urls_show.ejs in views
+  res.render("urls_show", templateVars);
+});
 
 // This tells our server to listen on our port
 app.listen(PORT, () => {
