375a3659a9614d1335e4f0f09802be0102ff3ca0
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-07 15:25:36 -0600

    added new users database

--- express_server.js
+++ express_server.js
@@ -15,7 +15,16 @@ app.set("view engine", "ejs");
 // ---- Databases ----
 // 
 const users = {
-  
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
 };
 
 const urlDatabase = {
